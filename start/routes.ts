884d651d1e25cc7df8d2aa4f73dfb9c2a1d6acd0
Udoh Lawrence <lawrenceaudoh@gmail.com>
2024-08-19 22:05:41 +0100

    create the registration/login pages

--- start/routes.ts
+++ start/routes.ts
@@ -1,12 +1,17 @@
-/*
-|--------------------------------------------------------------------------
-| Routes file
-|--------------------------------------------------------------------------
-|
-| The routes file is used for defining the HTTP routes.
-|
-*/
-
+const RegisterController = () => import('#controllers/register_controller')
+const LoginController = () => import('#controllers/login_controller')
 import router from '@adonisjs/core/services/router'
 
 router.on('/').render('pages/home')
+
+router.get('/login', [LoginController, 'show'])
+
+router.get('/register', [RegisterController, 'show'])
+
+router.post('/login', () => {
+  return 'Form Submitteed'
+})
+
+router.post('/register', () => {
+  return 'Form Submitteed'
+})
